858796f562f9a9c9dc8c177651b17c0036a600a3
Dougotavio <rm101117@estudante.fieb.edu.br>
2026-06-29 20:07:54 -0300

    Add files via upload

---
+++ usuaios/src/main/java/com/usuaios/UsuaiosApplication.java
@@ -0,0 +1,13 @@
+package com.usuaios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuaiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuaiosApplication.class, args);
+	}
+
+}
---
+++ usuaios/src/test/java/com/usuaios/UsuaiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuaios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuaiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
